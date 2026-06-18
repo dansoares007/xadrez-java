c973fe24acfda609cbe1e168ecee2e14b71d9af6
Daniel Soares <danielsantosmarinho402@gmail.com>
2026-06-18 14:37:23 -0300

    Adição da estrutura da Position e arquivo ux

--- src/Board/Position.java
+++ src/Board/Position.java
@@ -4,6 +4,34 @@ public class Position {
     private int column;
     private int row;
 
-    public void setValues(int row, int column){}
-    
+    public Position(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public void setValues(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
 }
