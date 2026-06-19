e65068af0d08798cff2cc56ddbf26d5283b05ece
Daniel Soares <danielsantosmarinho402@gmail.com>
2026-06-19 09:30:41 -0300

    Edição no Board

--- src/Chess/ChessException.java
+++ src/Chess/ChessException.java
@@ -3,7 +3,6 @@ package Chess;
 import Board.BoardException;
 
 public class ChessException extends BoardException {
-    private static final long serialVersionUID = 1L;
 
     public ChessException(String msg) {
         super(msg);
