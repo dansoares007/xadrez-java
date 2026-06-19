b0b9562615a3578d2fdec1a6d4d482b9a5d2dd87
Daniel Soares <danielsantosmarinho402@gmail.com>
2026-06-18 22:04:04 -0300

    Adição do ChessPosition

--- src/Chess/ChessPosition.java
+++ src/Chess/ChessPosition.java
@@ -1,4 +1,34 @@
 package Chess;
 
+import Board.Position;
+
 public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row) {
+
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    // Métodos protegidos de conversão interna matriz <-> xadrez (#)
+    protected Position toPosition() {
+
+    }
+
+    protected static ChessPosition fromPosition(Position position) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "" + column + row;
+    }
 }
