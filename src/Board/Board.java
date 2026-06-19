56d530903dc4e9cf4215eef3e765de4c56da2ae0
Daniel Soares <danielsantosmarinho402@gmail.com>
2026-06-18 21:37:43 -0300

    Adição do Board

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -1,4 +1,48 @@
-package Board;
+ package Board;
 
 public class Board {
+    private int rows;
+    private int columns;
+    private Piece[][] pieces;
+
+    public Board(int rows, int columns) {
+
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public Piece piece(int row, int column) {
+
+    }
+
+    public Piece piece(Position position) {
+
+    }
+
+    public void placePiece(Piece piece, Position position) {
+
+    }
+
+    public Piece removePiece(Position position) {
+
+    }
+
+    private boolean positionExists(int row, int column) {
+
+    }
+
+    public boolean positionExists(Position position) {
+
+    }
+
+    public boolean thereIsAPiece(Position position) {
+    }
 }
+
+
